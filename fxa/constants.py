e8f156a5e6cd24bf011cd1579c28b21425b5eb52
Michael Kane Juncker <michael.juncker@enote.com>
2021-03-03 17:20:38 +0100

    Open up constants for self-hosting

--- fxa/constants.py
+++ fxa/constants.py
@@ -1,27 +1,29 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
+from os import environ as env
+
 ENVIRONMENT_URLS = {
     "production": {
-        "authentication": "https://api.accounts.firefox.com/v1",
-        "oauth": "https://oauth.accounts.firefox.com/v1",
-        "content": "https://accounts.firefox.com/",
-        "profile": "https://profile.accounts.firefox.com/v1",
-        "token": "https://token.services.mozilla.com/",
+        "authentication": env.get("PROD_AUTH_URL", "https://api.accounts.firefox.com/v1"),
+        "oauth": env.get("PROD_OAUTH_URL", "https://oauth.accounts.firefox.com/v1"),
+        "content": env.get("PROD_CONTENT_URL", "https://accounts.firefox.com/"),
+        "profile": env.get("PROD_PROFILE_URL", "https://profile.accounts.firefox.com/v1"),
+        "token": env.get("PROD_TOKEN_URL", "https://token.services.mozilla.com/"),
     },
     "stage": {
-        "authentication": "https://api-accounts.stage.mozaws.net/v1",
-        "oauth": "https://oauth.stage.mozaws.net/v1",
-        "content": "https://accounts.stage.mozaws.net/",
-        "profile": "https://profile.stage.mozaws.net/v1",
-        "token": "https://token.stage.mozaws.net/",
+        "authentication": env.get("STAGE_AUTH_URL", "https://api-accounts.stage.mozaws.net/v1"),
+        "oauth": env.get("STAGE_OAUTH_URL", "https://oauth.stage.mozaws.net/v1"),
+        "content": env.get("STAGE_CONTENT_URL", "https://accounts.stage.mozaws.net/"),
+        "profile": env.get("STAGE_PROFILE_URL", "https://profile.stage.mozaws.net/v1"),
+        "token": env.get("STAGE_TOKEN_URL", "https://token.stage.mozaws.net/"),
     },
     "stable": {
-        "authentication": "https://stable.dev.lcip.org/auth/v1",
-        "oauth": "https://oauth-stable.dev.lcip.org/v1",
-        "content": "https://stable.dev.lcip.org/",
-        "profile": "https://stable.dev.lcip.org/profile/v1",
-        "token": None,
+        "authentication": env.get("STABLE_AUTH_URL", "https://stable.dev.lcip.org/auth/v1"),
+        "oauth": env.get("STABLE_OAUTH_URL", "https://oauth-stable.dev.lcip.org/v1"),
+        "content": env.get("STABLE_CONTENT_URL", "https://stable.dev.lcip.org/"),
+        "profile": env.get("STABLE_PROFILE_URL", "https://stable.dev.lcip.org/profile/v1"),
+        "token": env.get("STABLE_TOKEN_URL", None),
     }
 }
 
